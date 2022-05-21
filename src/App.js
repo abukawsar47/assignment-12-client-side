8ef6d8cd5af0b2122e64f5de3ab1352044904720
abukawsar47 <abukawsar47ak@gmail.com>
2022-05-22 01:14:29 +0600

    navbar added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
-import logo from './logo.svg';
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import Home from './Pages/Home/Home';
+import Navbar from './Pages/Shared/Navbar';
 
 function App() {
   return (
     <div>
-      <h2>Project start</h2>
+      <Navbar></Navbar>
+      <div className='max-w-7xl mx-auto px-12'>
+        <Routes>
+          <Route path='/' element={<Home />}></Route>
+        </Routes>
+      </div>
     </div>
   );
 }
