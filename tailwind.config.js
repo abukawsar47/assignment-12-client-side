eddf13bb6db53e6db0d9747fbee548d173ce22d4
abukawsar47 <abukawsar47ak@gmail.com>
2022-05-22 00:13:35 +0600

    daisy UI implement

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,5 +3,20 @@ module.exports = {
   theme: {
     extend: {},
   },
-  plugins: [],
+  daisyui: {
+    themes: [
+      {
+        lookUpTheme: {
+          primary: "#E40C2B",
+          secondary: "#1D1D2C",
+          accent: "#37cdbe",
+          neutral: "#3d4451",
+          "base-100": "#ffffff",
+        },
+      },
+      "dark",
+      "cupcake",
+    ],
+  },
+  plugins: [require("daisyui")],
 }
